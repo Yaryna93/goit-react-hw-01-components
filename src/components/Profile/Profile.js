24230dac62fa95e509353d566eb8736c5d950db8
Yaryna93 <yarynazezekalo@gmail.com>
2023-08-20 17:10:23 +0100

    updates

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+import style from './Profile.module.css';
+
+export const Profile = ({username,tag,location,avatar,stats}) =>{
+  return (
+  <div className={style.profile}>
+<div className={style.description}>
+  <img
+    src={avatar}
+    alt="User avatar"
+    className={style.avatar}
+  />
+  <p className={style.descItem}>{username}</p>
+  <p className={style.descItem}>@{tag}</p>
+  <p className={descItem}>{location}</p>
+</div>
+
+<ul className={style.stats}>
+  <li className={style.list}>
+    <span className={style.label}>Followers</span>
+    <span className={style.quantity}>1000</span>
+  </li>
+  <li className={style.list}>
+    <span className={style.label}>Views</span>
+    <span className={style.quantity}>2000</span>
+  </li>
+  <li className={style.list}>
+    <span className={style.label}>Likes</span>
+    <span className={style.quantity}>3000</span>
+  </li>
+</ul>
+</div>)}
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import style from './Statistics.module.css';
+
+export const Statistics=({title,stats})=>{
+  return (
+   <section className={style.statistics}>
+    <h2 className={style.title}>{title}</h2>
+  
+    <ul className={style.stat-list}>
+      {stats.map(({id, label, percentage})=>{
+return (
+  <li key={id}>
+    <span>{label}</span>
+    <span>{percentage}%</span>
+  </li>
+);
+})};
+</ul>
+</section>
+  );
+};
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired,
+      })
+    ),
+} ;
